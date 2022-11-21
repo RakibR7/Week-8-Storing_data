dc2db455045ba36ba9d6992a918add1d0132a8b3
Rakib <G00408644@atu.ie>
2022-11-21 10:19:47 +0000

    Added formating

--- src/main/java/ie/atu/Storing_data/Storing_Data.java
+++ src/main/java/ie/atu/Storing_data/Storing_Data.java
@@ -1,32 +1,28 @@
 package ie.atu.Storing_data;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Storing_Data {
     public static void main(String[] args) {
 
         {
             File myFile = new File( "MyFile.txt");
-            System.out.println("My file is located at "+ myFile.getAbsolutePath());
-
+            System.out.println("My file is located at"+ myFile.getAbsolutePath());
             try {
-                    FileWriter myWriter = new FileWriter(myFile, true);
-                    myWriter.write("This is my second file");
-                    myWriter.close();
+                PrintWriter MyWriter = new PrintWriter(
+                    new FileWriter("New Line"),true);
+
+                FileWriter myWriter = new FileWriter(myFile, true);
+                myWriter.write("This is a another file\t\n");
+                myWriter.close();
             }
             catch(IOException e)
             {
                 e.printStackTrace();
             }
 
-            //Writer(Object lock)
-
-
 
 
         }
-
     }
 }
