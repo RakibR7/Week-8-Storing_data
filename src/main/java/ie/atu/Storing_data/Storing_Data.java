5afb0aaa0a6d42816e950f524fc6a34a46a03e42
Rakib <G00408644@atu.ie>
2022-11-21 09:51:37 +0000

    Writing to file working

--- src/main/java/ie/atu/Storing_data/Storing_Data.java
+++ src/main/java/ie/atu/Storing_data/Storing_Data.java
@@ -1,5 +1,8 @@
 package ie.atu.Storing_data;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Storing_Data {
     public static void main(String[] args) {
@@ -7,6 +10,20 @@ public class Storing_Data {
         {
             File myFile = new File( "MyFile.txt");
             System.out.println("My file is located at "+ myFile.getAbsolutePath());
+
+            try {
+                FileWriter myWriter = new FileWriter(myFile);
+                myWriter.write("This is my first file");
+                myWriter.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+
+
+
+
         }
 
     }
