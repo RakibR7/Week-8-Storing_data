38f2c21a699d2354cd03243af7824286b2dda2b3
Rakib <G00408644@atu.ie>
2022-11-21 11:13:11 +0000

    Input to find files completed

--- src/main/java/ie/atu/Storing_data/Storing_Data.java
+++ src/main/java/ie/atu/Storing_data/Storing_Data.java
@@ -1,28 +1,39 @@
 package ie.atu.Storing_data;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Storing_Data {
     public static void main(String[] args) {
-
         {
-            File myFile = new File( "MyFile.txt");
-            System.out.println("My file is located at"+ myFile.getAbsolutePath());
+            File myFile = new File("Myfile.txt");
+            System.out.println("My file is located at" + myFile.getAbsolutePath());
+            String content = "Details to write";
+            /*
             try {
                 PrintWriter MyWriter = new PrintWriter(
-                    new FileWriter("New Line"),true);
-
+                        new FileWriter("New Line"), true);
                 FileWriter myWriter = new FileWriter(myFile, true);
                 myWriter.write("This is a another file\t\n");
                 myWriter.close();
-            }
-            catch(IOException e)
-            {
+            */
+
+                Scanner keyboard = new Scanner(System.in);
+                System.out.print("Enter filename: ");
+                String filename = keyboard.nextLine();
+
+                try (Scanner inputfile = new Scanner(new File(filename)))
+                {
+                    while(inputfile.hasNext())
+                    {
+                        String str = inputfile.nextLine();
+                        System.out.println(str);
+                    }
+                }
+                catch (FileNotFoundException e)
+                     {
                 e.printStackTrace();
             }
-
-
-
         }
     }
 }
